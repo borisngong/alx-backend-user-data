7da2a630625e1292903087dbd60a13fc3a0f54a6
borisngong <borisngong@gmail>
2024-11-07 03:56:03 -0800

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+#!?usr/bin/env python3
+""" """
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
+    return re.sub(pattern, lambda match: match.group().split('=')[0] + '='
+                  + redaction, message)
