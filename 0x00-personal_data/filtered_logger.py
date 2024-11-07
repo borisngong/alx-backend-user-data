2da81bc6fd325ca249336fd7c761b7fdca421b52
borisngong <borisngong@gmail>
2024-11-07 06:58:30 -0800

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,23 +1,60 @@
 #!/usr/bin/env python3
-"""A module for filtering logs.
-"""
+"""A module for filtering logs."""
 import os
 import re
 import logging
 from typing import List
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class for sensitive data"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        # Format the log record message using the base class format
+        original_message = super().format(record)
+        # Redact sensitive information
+        redacted_message = filter_datum(self.fields,
+                                        self.REDACTION,
+                                        original_message,
+                                        self.SEPARATOR)
+        return redacted_message
+
+
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+PII_FIELDS = ("email", "phone", "ssn", "name", "password")
 
 
-def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str,
-        ) -> str:
-    """Filters a log line.
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Redacts sensitive fields in a log message.
     """
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+def get_logger() -> logging.Logger:
+    """Creates a logger for user data with a RedactingFormatter."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    # Create a stream handler with the RedactingFormatter
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    # Attach the handler to the logger
+    logger.addHandler(stream_handler)
+
+    return logger
