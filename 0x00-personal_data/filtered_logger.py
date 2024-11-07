60028c48a62a59b8714807c6d19f3765bda85da4
borisngong <borisngong@gmail>
2024-11-07 07:10:19 -0800

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -37,7 +37,8 @@ patterns = {
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
     Responsible for Filtering a log line.
     """
