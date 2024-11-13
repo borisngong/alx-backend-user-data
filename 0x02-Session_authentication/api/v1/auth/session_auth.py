3b6a8f6fbcb55f3480df1e507ef725762eca5902
borisngong <borisngong@gmail>
2024-11-13 14:59:33 -0800

    Session Authentication Updated

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,7 +25,7 @@ class SessionAuth(Auth):
         # Store the session ID in the dictionary
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
         Retrieves the user ID based on a session ID
