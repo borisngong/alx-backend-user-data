3a27d75de65bf3919461a6d9063ea3187ecdeca1
borisngong <borisngong@gmail>
2024-11-20 06:05:25 -0800

    Create User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,17 +6,17 @@ Module for working with DB
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import SQLAlchemyError
 from user import Base, User
 
 
 class DB:
     """
-    DB class
+    DB class for managing database operations.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """Initialize a new DB instance."""
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -24,8 +24,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -33,6 +32,17 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Save the user to the database
+
+        Args:
+            email (str): The user's email
+            hashed_password (str): The user's hashed password
+
+        Returns:
+            User: The created User object
+
+        Raises:
+            SQLAlchemyError: If there is an error during
+            the database operation
         """
         try:
             user = User(email=email, hashed_password=hashed_password)
