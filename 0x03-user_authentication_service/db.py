d0bb458871383b5e00c8b6f4e9f0d83139fa91e8
borisngong <borisngong@gmail>
2024-11-20 06:09:13 -0800

    Create User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,54 +1,45 @@
 #!/usr/bin/env python3
+"""DB module.
 """
-Module for working with DB
-"""
-
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import SQLAlchemyError
+
 from user import Base, User
 
 
 class DB:
-    """
-    DB class for managing database operations.
+    """DB class.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance."""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """Initialize a new DB instance.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object."""
+        """Memoized session object.
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Save the user to the database
-
-        Args:
-            email (str): The user's email
-            hashed_password (str): The user's hashed password
-
-        Returns:
-            User: The created User object
-
-        Raises:
-            SQLAlchemyError: If there is an error during
-            the database operation
+        """Adds a new user to the database.
         """
         try:
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
             self._session.commit()
-            return user
-        except SQLAlchemyError as error:
+        except Exception:
             self._session.rollback()
-            raise error
+            new_user = None
+        return new_user
