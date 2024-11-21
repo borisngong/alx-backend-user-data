9b1b1453924d31a56fb178a1239d2afbd28320bd
borisngong <borisngong@gmail>
2024-11-21 10:33:22 -0800

    Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -99,5 +99,17 @@ def get_reset_password_token():
     return reset_tok
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(email, reset_token, new_password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
