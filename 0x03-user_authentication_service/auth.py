9aa1fc3c5a170e4fc150d92b046df000f6f2b408
borisngong <borisngong@gmail>
2024-11-20 18:04:00 -0800

    User Authenication Update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,15 +75,14 @@ class Auth:
         self._db.update_user(sp_user.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+    def get_user_from_session_id(self, session_id: str):
         """
-        Responsible for retrieving a user based on a given session ID
+        Find user by session ID
         """
-        user = None
         if session_id is None:
             return None
-        try:
-            user = self._db.find_user_by(session_id=session_id)
-        except NoResultFound:
-            return None
-        return user
+
+        # Assuming self._db has a method to get user by session ID
+        user = self._db.get_user_by_session_id(session_id)
+
+        return user if user else Nonessss
